ff18d6acec1e43a3e48999308705dadf7341d089
singodimejo <andira.a2n@gmail.com>
2024-02-04 15:08:30 +0700

    feat: Add Mockup Around App
    Sidebar for page navigation added.
    Logo from PPN added.

---
+++ src/lib/components/Sidebar.svelte
@@ -0,0 +1,54 @@
+<script lang="ts">
+	import { cn } from "$lib/utils";
+	import { Button } from "$lib/components/ui/button";
+
+	let className: string | null | undefined = undefined;
+	export { className as class };
+</script>
+
+<div class={cn("pb-12 border border-r min-h-screen", className)}>
+	<div class="space-y-4 py-4">
+        <a href="/" class="p-2 flex justify-center items-center hover:bg-secondary">
+            <img src="/PT_Pertamina_Patra_Niaga.svg" class="w-3/4" alt="">
+        </a>
+		<div class="px-3 py-2">
+			<h2 class="mb-2 px-4 text-lg font-semibold tracking-tight">Projects</h2>
+			<div class="space-y-1">
+                <Button variant='ghost' class='w-full justify-start'>
+                    New Project
+                </Button>
+                <Button variant='ghost' class='w-full justify-start'>
+                    Existing Projects
+                </Button>
+			</div>
+		</div>
+        <div class="px-3 py-2">
+			<h2 class="mb-2 px-4 text-lg font-semibold tracking-tight">RKS</h2>
+			<div class="space-y-1">
+                <Button variant='ghost' class='w-full justify-start'>
+                    Plan
+                </Button>
+                <a href="/cpm">
+                    <Button variant='ghost' class='w-full justify-start'>
+                        Schedule
+                    </Button>
+                </a>
+                <Button variant='ghost' class='w-full justify-start'>
+                    Draft
+                </Button>
+			</div>
+		</div>
+        <div class="px-3 py-2">
+			<h2 class="mb-2 px-4 text-lg font-semibold tracking-tight">OE</h2>
+			<div class="space-y-1">
+                <!-- OE Sub Menus Here -->
+			</div>
+		</div>
+        <div class="px-3 py-2">
+			<h2 class="mb-2 px-4 text-lg font-semibold tracking-tight">Pakta Integritas</h2>
+			<div class="space-y-1">
+                <!-- Pakta Integritas Sub Menus Here -->
+			</div>
+		</div>
+	</div>
+</div>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,1 +1,11 @@
-<script>import "../app.pcss";</script><slot></slot>
+<script>
+    import "../app.pcss";
+    import Sidebar from "$lib/components/Sidebar.svelte";
+</script>
+
+<div class="grid grid-cols-5">
+    <Sidebar />
+    <div class="col-span-4">
+        <slot></slot>
+    </div>
+</div>
